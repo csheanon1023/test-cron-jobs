224577bb71cfaad007e08e8ced2c372d6faf166f
csheanon1023 <87538841+csheanon1023@users.noreply.github.com>
2021-09-06 03:24:19 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -26,39 +26,56 @@ client.login(process.env.DISCORDJS_BOT_TOKEN).then(async () => {
   channel.send(`${getCurrentTime()} Process started`);
 });
 
-let count = 0;
-let count1 = 0;
-let flag = true;
-let flag1 = true;
+const channel;
+(async () => {
+  const channel = await client.channels.fetch('883775161478832169');
+});
+
+let countPre = 0;
+let countPost = 0;
+let flagPre = true;
+let flagPost = true;
 
 cron.schedule('50-59 9 * * 0,1,5,6', async () => {
-  if(flag) {
+  if(flagPre) {
     var currentdate = getCurrentTime();
     console.log("At each minute between 50-59 of the hours 9:00 on Sun, Mon, Fri, and Sat");
-    count ++;
-    if(count > 5)
-      flag = flase;
-    const channel = await client.channels.fetch('883775161478832169');
+    countPre ++;
+    if(countPre > 5)
+      flagPre = flase;
     channel.send(`\`\`\`${currentdate} \nAt each minute between 50-59 of the hours 9:00 on Sun, Mon, Fri, and Sat\`\`\``);
   }
   else {
-    const channel = await client.channels.fetch('883775161478832169');
     channel.send(`${getCurrentTime()} Didn't run`);
   }
 });
 
 cron.schedule('0-10 10 * * 0,1,5,6', async () => {
-  if(flag1) {
+  if(flagPost) {
     var currentdate = getCurrentTime();
     console.log("At each minute between 00-10 of the hours 10:00 on Sun, Mon, Fri, and Sat");
-    count1 ++;
-    if(count1 > 5)
-      flag = flase;
-    const channel = await client.channels.fetch('883775161478832169');
+    countPost ++;
+    if(countPost > 5)
+      flagPost = flase;
     channel.send(`\`\`\`${currentdate} \nAt each minute between 00-10 of the hours 10:00 on Sun, Mon, Fri, and Sat\`\`\``);
   }
   else {
-    const channel = await client.channels.fetch('883775161478832169');
+    channel.send(`${getCurrentTime()} Didn't run`);
+  }
+});
+
+let c1 = 0;
+let f1 = true;
+cron.schedule('* * * * *', async () => {
+  if(f1) {
+    var currentdate = getCurrentTime();
+    console.log("At each minute between 00-10 of the hours 10:00 on Sun, Mon, Fri, and Sat");
+    c1 ++;
+    if(c1 > 5)
+      f1 = flase;
+    channel.send(`\`\`\`${currentdate} \nAt each minute between 00-10 of the hours 10:00 on Sun, Mon, Fri, and Sat\`\`\``);
+  }
+  else {
     channel.send(`${getCurrentTime()} Didn't run`);
   }
 });
